bab92b90b06dff6ce29cc1a7921a9861848ba8b7
Itay Komemy <itay.komemy@gmail.com>
2018-04-06 14:43:36 +0300

    Reset DonorLoader after fetching all donors

--- ui/src/DonorLoader.js
+++ ui/src/DonorLoader.js
@@ -2,9 +2,18 @@ import * as api from './api'
 import {MOCK} from './Config'
 
 let lastLoaded = 0
+let totalCount = null
 
 export const fetchDonors = (count) => {
     const startAt = lastLoaded
     lastLoaded += count
-    return api.fetchDonors(startAt, count)
+    if (lastLoaded > totalCount) {
+        lastLoaded = 0
+    }
+    const p = api.fetchDonors(startAt, count)
+    p.then(({count}) => {
+        totalCount = count
+    })
+
+    return p
 }
