bf2f4fd32f97b66df7fae5b9f6d7d64d2e591a80
Itay Komemy <itay.komemy@gmail.com>
2018-05-20 00:57:47 +0300

    Remove unused MOCK env-var

--- ui/src/Config.js
+++ ui/src/Config.js
@@ -1,9 +1,4 @@
-export const MOCK = process.env.REACT_APP_USE_MOCK === "true"
 export const DEBUG = process.env.REACT_APP_DEBUG === "true"
-
-if (MOCK) {
-    console.warn('MOCK is True')
-}
 
 if (DEBUG) {
     console.warn('DEBUG is True')
--- ui/src/DonorLoader.js
+++ ui/src/DonorLoader.js
@@ -1,6 +1,4 @@
 import * as api from './api'
-import {MOCK} from './Config'
-
 
 export const fetchDonors = (x0, x1, y0, y1) =>
     api.fetchDonors(x0, x1, y0, y1)
